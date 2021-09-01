c7e6984621e1aef83259ca9b21951410ccfbf34c
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-01 14:17:29 -0500

    added get route for single select company and refactored get all companies route

--- routes/companies.js
+++ routes/companies.js
@@ -2,10 +2,31 @@ const express = require('express')
 const router = new express.Router()
 const ExpressError = require('../expressError')
 const db = require('../db')
+const { route } = require('../app')
+const { arrowFunctionExpression } = require('@babel/types')
 
-router.get('/', async (req, res) => {
-  const results = await db.query('SELECT * FROM companies')
-  return res.status(200).json({ companies: results.rows })
+/** Route for getting all companies from db */
+router.get('/', async (req, res, next) => {
+  try {
+    const results = await db.query('SELECT * FROM companies')
+    return res.status(200).json({ companies: results.rows })
+  } catch (err) {
+    return next(err)
+  }
 })
+
+/** Route for getting selected company from db by code */
+router.get('/:code', async (req, res, next) => {
+  try {
+    const { code } = req.params
+    const results = await db.query('SELECT * FROM companies WHERE code = $1', [code])
+    if (results.rows.length === 0) throw new ExpressError('Company not found', 404)
+    return res.status(200).json({ company: results.rows[0] })
+  } catch (err) {
+    return next(err)
+  }
+})
+
+
 
 module.exports = router
