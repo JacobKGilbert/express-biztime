6456ac7746db0d8ef55e3eee07b93d9f53dbe4f4
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-01 13:55:51 -0500

    added get all companies route

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,9 @@ const router = new express.Router()
 const ExpressError = require('../expressError')
 const db = require('../db')
 
-
+router.get('/', async (req, res) => {
+  const results = await db.query('SELECT * FROM companies')
+  return res.status(200).json({ companies: results.rows })
+})
 
 module.exports = router
