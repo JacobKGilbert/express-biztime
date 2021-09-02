632df30f1e0796f3c72b61c650ce4113aa549794
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-02 17:02:29 -0500

    added industry seed data

--- data.sql
+++ data.sql
@@ -2,6 +2,7 @@
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +20,17 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+  code text PRIMARY KEY,
+  industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE companies_industries (
+  comp_code text NOT NULL REFERENCES companies,
+  ind_code text NOT NULL REFERENCES industries,
+  PRIMARY KEY(comp_code, ind_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +40,8 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries
+  VALUES ('manu', 'manufacturing'),
+         ('comp', 'computer'),
+         ('cld', 'cloud computing');
