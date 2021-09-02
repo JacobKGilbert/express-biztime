d0a30f3f89b2086af721fcf82a238c45406bf617
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-02 11:27:04 -0500

    created get all companies test

---
+++ tests/companies.test.js
@@ -0,0 +1,43 @@
+process.env.NODE_ENV = 'test'
+
+const request = require('supertest')
+const app = require('../app')
+const db = require('../db')
+
+const APPLE = {
+  code: 'apl',
+  name: 'Apple',
+  description: 'iPhone maker'
+}
+const IBM = {
+  code: 'ibm',
+  name: 'IBM',
+  description: 'Data cloud company'
+}
+let testComp
+
+beforeEach(async () => {
+  const result = await db.query(
+    `INSERT INTO companies (code, name, description)
+      VALUES ($1, $2, $3)
+      RETURNING code, name, description`,
+      [APPLE.code, APPLE.name, APPLE.description]
+  )
+  testComp = result.rows[0]
+})
+
+afterEach(async () => {
+  await db.query('DELETE FROM companies')
+})
+
+afterAll(async () => {
+  await db.end()
+})
+
+describe('GET /companies', () => {
+  test('Get all companies', async () => {
+    const res = await request(app).get('/companies')
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({ companies: [testComp] })
+  })
+})
