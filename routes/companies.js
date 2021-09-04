31b7d20a2af73f358f80b30df4a99cf253718520
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-04 11:41:07 -0500

    added industry handling to get by code route

--- routes/companies.js
+++ routes/companies.js
@@ -33,16 +33,33 @@ router.post('/', async (req, res, next) => {
 router.get('/:code', async (req, res, next) => {
   try {
     const { code } = req.params
-    const compResult = await db.query('SELECT * FROM companies WHERE code = $1', [code])
+    const compResult = await db.query(
+      `SELECT c.code, c.name, c.description, i.industry
+      FROM companies AS c
+      LEFT JOIN companies_industries AS ci
+      ON c.code = ci.comp_code
+      LEFT JOIN industries AS i
+      ON ci.ind_code = i.code
+      WHERE c.code = $1`,
+      [code]
+    )
     const invResult = await db.query('SELECT id FROM invoices WHERE comp_code = $1', [code])
 
     if (compResult.rows.length === 0)
       throw new ExpressError('Company not found.', 404)
 
-    const company = compResult.rows[0]
-    const invoices = invResult.rows
-
-    company.invoices = invoices.map((inv) => inv.id)
+    const { name, description } = compResult.rows[0]
+    const company = {
+      code,
+      name,
+      description
+    }
+    const industries = compResult.rows.map((c) => c.industry)
+    if (industries[0] !== null) {
+      company.industries = industries
+    }
+    if (invResult.rows.length !== 0)
+      company.invoices = invResult.rows.map((inv) => inv.id)
 
     return res.status(200).json({ 'company': company })
   } catch (err) {
